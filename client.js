4528e9c7e71bb3ca6d22313bdb5e37b7adf015be
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-10 16:16:30 -0500

    Constants Module

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
+const { IP, PORT } = require('./constants');
 const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
+    host: IP,
+    port: PORT
   });
   conn.setEncoding('utf8');
   conn.on('data', (text) => {
