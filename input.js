de870ea45a703d5023a37fc85230c5943a549502
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-10 15:29:29 -0500

    WASD Movement

--- input.js
+++ input.js
@@ -1,17 +1,31 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  }
-  handleUserInput()
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
   return stdin;
+}
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
 };
 
 module.exports = {setupInput}
