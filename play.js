1897b449b32a167eb40215ec4f923304ba55492d
vbabison <bobby@Babisons-Mac-mini.local>
2021-02-10 13:44:19 -0500

    Stdin Setup

--- play.js
+++ play.js
@@ -2,3 +2,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+  handleUserInput()
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+setupInput()
+
